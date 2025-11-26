38c2ad2b47507de69ad42eceba8900c160a3d39e
ltfh-31 <ipehhh06@gmail.com>
2025-11-26 09:45:08 +0700

    Add files via upload

---
+++ isi.js
@@ -0,0 +1,7 @@
+document.writeln("<CENTER>");
+document.writeln("<HR WIDTH=600 COLOR=blue");
+document.writeln("<b><font size=20>SMK AL-GHIFARI BANYURESMI</font></b>");
+document.writeln("<h2>ILMU KOMPUTER</h2>");
+document.writeln("<h3>REKAYASA PERANGKAT LUNAK</h3>");
+document.writeln("<HR WIDTH=600 COLOR=blue>");
+document.writeln("</CENTER>");
